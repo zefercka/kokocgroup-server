4771eb8d04b6802733bd6d87247c15f2fd85ad5d
Sergey <xramtsoff2005@yandex.ru>
2024-10-08 14:19:36 +0300

    perm in user

--- app/api/schemas/user.py
+++ app/api/schemas/user.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, field_validator, model_validator
 from datetime import date
 from ..schemas.role import Role
 from datetime import datetime
@@ -22,6 +22,7 @@ class CreateUser(UserBase):
 class User(UserBase):
     id: int
     roles: list[Role]
+    permissions: list[str] = []
 
     class Config:
         from_attributes = True
@@ -29,6 +30,24 @@ class User(UserBase):
     @field_validator("roles", mode="before")
     def adjust_roles(roles):
         return [Role(id=role.id, name=role.name, permissions=role.permissions) for role in roles]
+    
+    @model_validator(mode="after")
+    def collect_permissions(cls, values):
+        roles = values.roles
+        if roles:
+            permissions = []
+            for role in roles:
+                permissions.extend(role.permissions)
+            values.permissions = list(set(permissions))
+        return values
+    
+    # @field_validator("permissions", "roles", mode="after")
+    # def adjust_permissions(roles):
+    #     permissions = []
+    #     for role in roles:
+    #         permissions.extend(role.permissions)
+    #     print(permissions)
+    #     return list(set(permissions))
         
 
 class AuthorizedUser(BaseModel):
